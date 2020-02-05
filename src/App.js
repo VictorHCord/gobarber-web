f711b18331bf8c0e184e7289019358ef1ef81619
Victor hugo cordeiro <victor.hugo@systemhope.com.br>
2020-02-04 22:31:45 -0300

    Configuração de rotas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+import Routes from './routes';
+import History from './services/history';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Router history={History}>
+      <Routes />
+    </Router>
   );
 }
 
