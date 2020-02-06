f94fdd58c1a501a3af24a323a0f237eccf29307b
Victor hugo cordeiro <victor.hugo@systemhope.com.br>
2020-02-05 21:07:30 -0300

    estilizando autenticação

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Router } from 'react-router-dom';
 import './config/ReactotronConfig';
 import Routes from './routes';
 import History from './services/history';
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <Router history={History}>
       <Routes />
+      <GlobalStyle />
     </Router>
   );
 }
--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import logo from '~/assets/logo.svg';
 // import { Container } from './styles';
 
 export default function SignIn() {
-  return <div>Logar</div>;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <form>
+        <input type="email" placeholder="Seu email" />
+        <input type="password" placeholder="Sua senha" />
+
+        <button type="submit"> Acessar </button>
+        <Link to="/register"> Criar conta </Link>
+      </form>
+    </>
+  );
 }
