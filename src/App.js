e6c1b3dccf4e50bf10b2ab401b07e27f3bf8db3b
Victor hugo cordeiro <victor.hugo@systemhope.com.br>
2020-02-16 19:53:49 -0300

    autenticação

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 import './config/ReactotronConfig';
 import Routes from './routes';
 import History from './services/history';
+import store from './store';
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
-    <Router history={History}>
-      <Routes />
-      <GlobalStyle />
-    </Router>
+    <Provider store={store}>
+      <Router history={History}>
+        <Routes />
+        <GlobalStyle />
+      </Router>
+    </Provider>
   );
 }
 
